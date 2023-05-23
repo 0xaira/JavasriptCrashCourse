e65c93516013d91eb93e8b2cf7cc75ca57d70687
Arsenic <airajena07@gmail.com>
2023-05-23 10:50:13 +0530

    Coding Challenge 1

--- L1/script.js
+++ L1/script.js
@@ -3,11 +3,26 @@ let str="LuciFeR";
 //     alert("Hello Lucifer");
 // }
 
-let age=19;
-const birthyear=2002;
-var name="Lucifer";
-console.log(age);
-console.log(birthyear);
-console.log(name);
+// let age=19;
+// const birthyear=2002;
+// var name="Lucifer";
+// console.log(age);
+// console.log(birthyear);
+// console.log(name);
+
+let markHeight=1.69;
+let markWeight=78;
+
+let johnHeight=1.95;
+let johnWeight=92;
+
+let markBMI=markWeight/(markHeight**2);
+let johnBMI=johnWeight/(johnHeight**2);
+if(markBMI>johnBMI){
+    console.log(`Mark's BMI ${markBMI} is higher than John's ${johnBMI}`);
+}
+else{
+    console.log(`John's BMI ${johnBMI} is higher than Mark's ${markBMI}`);
+}
 
 
