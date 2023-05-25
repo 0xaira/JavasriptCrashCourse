06ad721e5b1e50b06d08f8b3df5a9b8e661058dd
Arsenic <airajena07@gmail.com>
2023-05-25 17:59:56 +0530

    CS updates

--- L1/script.js
+++ L1/script.js
@@ -1,38 +1,29 @@
-// let str="LuciFeR";
-// // if(str==="LuciFeR"){
-// //     alert("Hello Lucifer");
-// // }
 
-// // let age=19;
-// // const birthyear=2002;
-// // var name="Lucifer";
-// // console.log(age);
-// // console.log(birthyear);
-// // console.log(name);
+let markHeight=1.69;
+let markWeight=78;
 
-// let markHeight=1.69;
-// let markWeight=78;
+let johnHeight=1.95;
+let johnWeight=92;
 
-// let johnHeight=1.95;
-// let johnWeight=92;
+let markBMI=markWeight/(markHeight**2);
+let johnBMI=johnWeight/(johnHeight**2);
+if(markBMI>johnBMI){
+    console.log(`Mark's BMI ${markBMI} is higher than John's ${johnBMI}`);
+}
+else{
+    console.log(`John's BMI ${johnBMI} is higher than Mark's ${markBMI}`);
+}
 
-// let markBMI=markWeight/(markHeight**2);
-// let johnBMI=johnWeight/(johnHeight**2);
-// if(markBMI>johnBMI){
-//     console.log(`Mark's BMI ${markBMI} is higher than John's ${johnBMI}`);
-// }
-// else{
-//     console.log(`John's BMI ${johnBMI} is higher than Mark's ${markBMI}`);
-// }
 
-// let tip= 500;
-// if(tip>=50 && tip<=300){
-//     console.log(`The bill was ${tip}, the tip was ${tip*0.15} and the total value ${tip*1.15}`);
-// }
-// else{
-//     console.log(`The bill was ${tip}, the tip was ${tip*0.2} and the total value ${tip*1.2}`);
-// }
-// tip>=50 && tip<=300?console.log(`The bill was ${tip}, the tip was ${tip*0.15} and the total value ${tip*1.15}`):console.log(`The bill was ${tip}, the tip was ${tip*0.2} and the total value ${tip*1.2}`);
+let tip= 500;
+if(tip>=50 && tip<=300){
+    console.log(`The bill was ${tip}, the tip was ${tip*0.15} and the total value ${tip*1.15}`);
+}
+else{
+    console.log(`The bill was ${tip}, the tip was ${tip*0.2} and the total value ${tip*1.2}`);
+}
+tip>=50 && tip<=300?console.log(`The bill was ${tip}, the tip was ${tip*0.15} and the total value ${tip*1.15}`):console.log(`The bill was ${tip}, the tip was ${tip*0.2} and the total value ${tip*1.2}`);
+
 
 let team1="Dolphins";
 let team2="Koalas";
