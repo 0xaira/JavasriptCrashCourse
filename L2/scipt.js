edf63932105b6727d1366a156dce42952c9bb901
Arsenic <airajena07@gmail.com>
2023-05-25 20:09:34 +0530

    CC #2

--- L2/scipt.js
+++ L2/scipt.js
@@ -131,6 +131,17 @@
 //     console.log('You have a friend called Steven');
 // }
 
+const calcTip=function (bill) {
+    return bill>=50 && bill<=300 ? bill*0.15 : bill*0.2;
+}
+const bills=[125,555,44];
+const tips=[calcTip(bills[0]),calcTip(bills[1]),calcTip(bills[bills.length-1])];
+console.log(bills,tips);
+const totals=[bills[0]+tips[0],bills[1]+tips[1],bills[2]+tips[2]];
+console.log(totals);
+
+
+
 
 
 
