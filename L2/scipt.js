3a1ef4d894fafb927fbcbf7ab374e3e5d30549b8
Arsenic <airajena07@gmail.com>
2023-05-25 18:36:42 +0530

    Arrow Functions

--- L2/scipt.js
+++ L2/scipt.js
@@ -7,15 +7,37 @@
 // juiceMaker(5,0);
 // console.log(juiceMaker(5,0));
 
-function calcAge1(birthYear) {
-    return 2021-birthYear;
-}
-let age1=calcAge1(1991);
-console.log(age1);
+// function calcAge1(birthYear) {
+//     return 2021-birthYear;
+// }
+// let age1=calcAge1(1991);
+// console.log(age1);
 
-const calcAge2=function (birthYear) { 
-    return 2021-birthYear;
+// const calcAge2=function (birthYear) { 
+//     return 2021-birthYear;
+// }
+// let age2=calcAge2(1991);
+// console.log(age2);
+
+// Arrow Function
+ const calcAge3=birthYear=>2021-birthYear;
+    let age3=calcAge3(1991);
+    console.log(age3);
+
+const yearsUntilRetirement=birthYear=>{
+    const age=2021-birthYear;
+    const retirement=65-age;
+    return retirement;
 }
-let age2=calcAge2(1991);
-console.log(age2);
+console.log(yearsUntilRetirement(1991));
+
+const yearsUntilRetirement1=(birthYear,firstName)=>{
+    const age=2021-birthYear;
+    const retirement=65-age;
+    return `${firstName} retires in ${retirement} years`;
+}
+console.log(yearsUntilRetirement1(1991,'Jonas'));
+
+
+
 
