d4397248c3ee47d2532a437cb867493dfa0cf297
Arsenic <airajena07@gmail.com>
2023-05-25 17:45:51 +0530

    modified

--- L1/script.js
+++ L1/script.js
@@ -1,28 +1,28 @@
-let str="LuciFeR";
-// if(str==="LuciFeR"){
-//     alert("Hello Lucifer");
+// let str="LuciFeR";
+// // if(str==="LuciFeR"){
+// //     alert("Hello Lucifer");
+// // }
+
+// // let age=19;
+// // const birthyear=2002;
+// // var name="Lucifer";
+// // console.log(age);
+// // console.log(birthyear);
+// // console.log(name);
+
+// let markHeight=1.69;
+// let markWeight=78;
+
+// let johnHeight=1.95;
+// let johnWeight=92;
+
+// let markBMI=markWeight/(markHeight**2);
+// let johnBMI=johnWeight/(johnHeight**2);
+// if(markBMI>johnBMI){
+//     console.log(`Mark's BMI ${markBMI} is higher than John's ${johnBMI}`);
 // }
-
-// let age=19;
-// const birthyear=2002;
-// var name="Lucifer";
-// console.log(age);
-// console.log(birthyear);
-// console.log(name);
-
-let markHeight=1.69;
-let markWeight=78;
-
-let johnHeight=1.95;
-let johnWeight=92;
-
-let markBMI=markWeight/(markHeight**2);
-let johnBMI=johnWeight/(johnHeight**2);
-if(markBMI>johnBMI){
-    console.log(`Mark's BMI ${markBMI} is higher than John's ${johnBMI}`);
-}
-else{
-    console.log(`John's BMI ${johnBMI} is higher than Mark's ${markBMI}`);
-}
+// else{
+//     console.log(`John's BMI ${johnBMI} is higher than Mark's ${markBMI}`);
+// }
 
 
