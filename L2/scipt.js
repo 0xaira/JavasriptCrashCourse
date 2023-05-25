53eced2f1957dee3ebad198ca24dec71661ab141
Arsenic <airajena07@gmail.com>
2023-05-25 18:29:57 +0530

    age function

--- L2/scipt.js
+++ L2/scipt.js
@@ -1,8 +1,21 @@
 //Functions
 
-function juiceMaker(apples,oranges) {
-    let juice=`Juice with ${apples} apples and ${oranges} oranges`;
-    return juice;
+// function juiceMaker(apples,oranges) {
+//     let juice=`Juice with ${apples} apples and ${oranges} oranges`;
+//     return juice;
+// }
+// juiceMaker(5,0);
+// console.log(juiceMaker(5,0));
+
+function calcAge1(birthYear) {
+    return 2021-birthYear;
 }
-juiceMaker(5,0);
-console.log(juiceMaker(5,0));
+let age1=calcAge1(1991);
+console.log(age1);
+
+const calcAge2=function (birthYear) { 
+    return 2021-birthYear;
+}
+let age2=calcAge2(1991);
+console.log(age2);
+
