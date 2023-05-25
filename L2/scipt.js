2b99e7e619a8c34981720dbe8acb6018e78c8119
Arsenic <airajena07@gmail.com>
2023-05-25 18:26:50 +0530

    Functions

---
+++ L2/scipt.js
@@ -0,0 +1,8 @@
+//Functions
+
+function juiceMaker(apples,oranges) {
+    let juice=`Juice with ${apples} apples and ${oranges} oranges`;
+    return juice;
+}
+juiceMaker(5,0);
+console.log(juiceMaker(5,0));
