85f8329c80b7189c8ebeaa6d6a491b8cb2547fba
Arsenic <airajena07@gmail.com>
2023-05-25 20:00:04 +0530

    Basic Functions

--- L2/scipt.js
+++ L2/scipt.js
@@ -113,6 +113,26 @@
 // const ages=[calcAge(years[0]),calcAge(years[1]),calcAge(years[years.length-1])];
 // console.log(ages);
 
+//  const friends=['Michael','Steven','Peter'];
+// const newLength=friends.push('Jay');
+// console.log(friends);
+// console.log(newLength);
+// friends.unshift('John');
+// console.log(friends);
+// friends.pop();
+// console.log(friends);
+// friends.shift();
+// console.log(friends);
+// console.log(friends.indexOf('Steven'));
+// console.log(friends.indexOf('Bob'));
+// console.log(friends.includes('Steven'));
+// console.log(friends.includes('Bob'));
+// if(friends.includes('Steven')){
+//     console.log('You have a friend called Steven');
+// }
+
+
+
 
 
 
