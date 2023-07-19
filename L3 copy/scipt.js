db1f542f251888211dc5a062d8cb3707236dd9b8
Arsenic <airajena07@gmail.com>
2023-07-19 15:50:41 +0530

    functions

--- L3 copy/scipt.js
+++ L3 copy/scipt.js
@@ -5,4 +5,25 @@ if(passTest)hasDriversLicense=true;
 if(hasDriversLicense)console.log('I can drive :D');
 // const interface='Audio';
 // const private=534;
+function logger(){
+    console.log('My name is Jonas');
+}
+// calling/running/invoking function
+logger();
+logger();
+
+function fruitProcessor(apples,oranges){
+
+    console.log(apples,oranges);
+    const juice=`Juice with ${apples} apples and ${oranges} oranges`;
+    return juice;
+}
+fruitProcessor(5,0);
+const appleJuice=fruitProcessor(5,0);
+console.log(appleJuice);
+console.log(fruitProcessor(5,0));
+
+const appleOrangeJuice=fruitProcessor(2,4);
+console.log(appleOrangeJuice);
+
 
