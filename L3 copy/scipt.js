4474017f7252b4147b130934ab2f6afb02aa2957
Arsenic <airajena07@gmail.com>
2023-07-19 15:57:13 +0530

    Arrow functions

--- L3 copy/scipt.js
+++ L3 copy/scipt.js
@@ -1,29 +1,69 @@
 'use strict';
-let hasDriversLicense=false;
-const passTest=true;
-if(passTest)hasDriversLicense=true;
-if(hasDriversLicense)console.log('I can drive :D');
-// const interface='Audio';
-// const private=534;
-function logger(){
-    console.log('My name is Jonas');
-}
-// calling/running/invoking function
-logger();
-logger();
+// let hasDriversLicense=false;
+// const passTest=true;
+// if(passTest)hasDriversLicense=true;
+// if(hasDriversLicense)console.log('I can drive :D');
+// // const interface='Audio';
+// // const private=534;
+// function logger(){
+//     console.log('My name is Jonas');
+// }
+// // calling/running/invoking function
+// logger();
+// logger();
 
-function fruitProcessor(apples,oranges){
+// function fruitProcessor(apples,oranges){
 
-    console.log(apples,oranges);
-    const juice=`Juice with ${apples} apples and ${oranges} oranges`;
-    return juice;
-}
-fruitProcessor(5,0);
-const appleJuice=fruitProcessor(5,0);
-console.log(appleJuice);
-console.log(fruitProcessor(5,0));
+//     console.log(apples,oranges);
+//     const juice=`Juice with ${apples} apples and ${oranges} oranges`;
+//     return juice;
+// }
+// fruitProcessor(5,0);
+// const appleJuice=fruitProcessor(5,0);
+// console.log(appleJuice);
+// console.log(fruitProcessor(5,0));
 
-const appleOrangeJuice=fruitProcessor(2,4);
-console.log(appleOrangeJuice);
+// const appleOrangeJuice=fruitProcessor(2,4);
+// console.log(appleOrangeJuice);
 
+// function calcAge1(birthYear){
+//     return 2021-birthYear;
+// }
+// const age1=calcAge1(1991);
+// console.log(age1);
+
+// const calcAge2=function(birthYear){
+//     return 2021-birthYear;
+// }
+// const age2=calcAge2(1991);
+// console.log(age2);
+
+// const calcAge3=birthYear=>2021-birthYear;
+// const age3=calcAge3(1991);
+// console.log(age3);
+
+// const yearsUntilRetirement=birthYear=>{
+//     const age=2021-birthYear;
+//     const retirement=65-age;
+//     return retirement;
+// }
+// console.log(yearsUntilRetirement(1991));
+
+// const yearsUntilRetirement1=(birthYear,firstName)=>{
+//     const age=2021-birthYear;
+//     const retirement=65-age;
+//     return `${firstName} retires in ${retirement} years`;
+// }
+// console.log(yearsUntilRetirement1(1991,'Jonas'));
+
+// function cutFruitPieces(fruit){
+//     return fruit*4;
+// }
+// function fruitProcessor(apples,oranges){
+//     const applePieces=cutFruitPieces(apples);
+//     const orangePieces=cutFruitPieces(oranges);
+//     const juice=`Juice with ${applePieces} pieces of apple and ${orangePieces} pieces of orange`;
+//     return juice;
+// }
+// console.log(fruitProcessor(2,3));
 
