5e32e1a1dc67c54f1b9dde301c57aeb34f98fdbc
Arsenic <airajena07@gmail.com>
2023-07-20 16:47:00 +0530

    query selector

--- L7 copy/script.js
+++ L7 copy/script.js
@@ -1,1 +1,27 @@
 'use strict';
+console.log(document.querySelector('.message').textContent);
+document.querySelector('.message').textContent = 'Correct Number!🎉';
+console.log(document.querySelector('.message').textContent);
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 10;
+document.querySelector('.guess').value = 23;
+console.log(document.querySelector('.guess').value);
+// Path: L8 handling click events
+document.querySelector('.check').addEventListener('click', function () {
+    const guess = Number(document.querySelector('.guess').value);
+    console.log(guess, typeof guess);
+    // When there is no input
+    if (!guess) {
+        document.querySelector('.message').textContent = 'No Number!😥';
+    }
+    }
+    // When player wins
+    else if (guess === secretNumber) {
+        document.querySelector('.message').textContent = 'Correct Number!🎉';
+    }
+    // When guess is too high
+    else if (guess > secretNumber) {
+        if (score > 1) {
+            document.querySelector('.message').textContent = 'Too High!📈';
+            score--;
+            
