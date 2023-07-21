daea0fbdf230cbdbf71018172b4524b59f5f5bc6
Arsenic <airajena07@gmail.com>
2023-07-21 17:18:20 +0530

    variables

---
+++ Javascript/Basics/script.js
@@ -0,0 +1,8 @@
+const account_name = "John Doe";
+let accountID = 123456;
+let accountBalance = 1000.00;
+let accountActive = true;
+let accountAge = null;
+let accountAddress;
+console.log(account_name, accountID, accountBalance, accountActive, accountAge, accountAddress);
+console.table({ account_name, accountID, accountBalance, accountActive, accountAge, accountAddress });
