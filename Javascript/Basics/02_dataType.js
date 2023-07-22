ea8367bb680a8a7abdf85e507d68f0678a6bf601
Arsenic <airajena07@gmail.com>
2023-07-22 18:03:33 +0530

    Basics

---
+++ Javascript/Basics/02_dataType.js
@@ -0,0 +1,26 @@
+"use strict"; // Treat all JS Code as newer version of JS
+
+//Focus on code readability
+//Use camelCase for variable names
+//Use PascalCase for class names
+//Use snake_case for function names
+//Use UPPERCASE for constants
+
+let name = "John Doe";
+let age = 30;
+let isApproved = true;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => uniqueness
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
+
