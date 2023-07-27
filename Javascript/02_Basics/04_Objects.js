a2af5525721447ce0d4d2f0017c91f16511a6fcc
Arsenic <airajena07@gmail.com>
2023-07-27 17:21:09 +0530

    Objects

---
+++ Javascript/02_Basics/04_Objects.js
@@ -0,0 +1,74 @@
+const tinderProfile = {
+    name: 'Elon Musk',
+    age: 45,
+    hobbies: ['Coding', 'Cricket', 'Gaming'],
+    isOnline: true,
+    lastSeen: '2021-07-07T09:44:27.000Z',
+} 
+console.log(tinderProfile);
+
+//Creating Singleton Objects using new keyword
+let myCar = new Object();
+myCar.make = "Ford";
+myCar.model = "Mustang";
+myCar.year = 1969;
+myCar.color = "red";
+console.log(myCar);
+console.log(myCar.make);
+console.log(myCar["model"]);
+myCar.color = "blue";
+console.log(myCar.color); 
+
+//Objects inside Objects
+// let myCar2 = new Object();
+// myCar2.make = "Ford";
+// myCar2.model = "Mustang";
+// myCar2.year = 1969;
+// myCar2.color = "red";
+// myCar2.engine = new Object();
+// myCar2.engine.size = 5.0;
+// myCar2.engine.make = "bmw";
+// myCar2.engine.fuel = "petrol";
+// myCar2.engine.pistons = new Array();
+// myCar2.engine.pistons[0] = new Object();
+// myCar2.engine.pistons[0].maker = "BMW";
+// myCar2.engine.pistons[1] = new Object();
+// myCar2.engine.pistons[1].maker = "BMW2";
+// console.log(myCar2);
+// console.log(myCar2.engine.pistons[0].maker);
+// console.log(myCar2.engine.pistons[1].maker);
+
+//Obkects inside Objects non singleton
+ let marvel_heros = new Object();
+    marvel_heros.thor = "Thor";
+    marvel_heros.ironman = "Ironman";
+    marvel_heros.spiderman = "Spiderman";
+    marvel_heros.drstrange = "DR Strange";
+    marvel_heros.hulk = "Hulk";
+    marvel_heros.captainamerica = "Captain America";
+    marvel_heros.blackwidow = "Black Widow";
+    marvel_heros.blackpanther = "Black Panther";
+    marvel_heros.antman = "Antman";
+    marvel_heros.wolverine = "Wolverine";
+    marvel_heros.deadpool = "Deadpool";
+    marvel_heros.groot = "Groot";
+    marvel_heros.rocket = "Rocket";
+    marvel_heros.gamora = "Gamora";
+    marvel_heros.starlord = "Starlord";
+    marvel_heros.drax = "Drax";
+
+    let dc_heros = new Object();
+    dc_heros.superman = "Superman";
+    dc_heros.batman = "Batman";
+    dc_heros.flash = "Flash";
+    dc_heros.joker = "Joker";
+    dc_heros.aquaman = "Aquaman";
+
+    let heros = new Object();
+    heros.marvel = marvel_heros;
+    heros.dc = dc_heros;
+    console.log(heros);
+    console.log(heros.marvel.thor);
+    console.log(heros.dc.superman);
+
+
