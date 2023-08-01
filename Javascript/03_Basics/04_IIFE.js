8bed3b8f81383631af46bc4187e37b7f769c2d09
Arsenic <airajena07@gmail.com>
2023-08-01 17:24:49 +0530

    4

---
+++ Javascript/03_Basics/04_IIFE.js
@@ -0,0 +1,13 @@
+// Immediatly Invoked Function Expression
+
+(function sum(a, b) {
+     let sum= a + b;      // Function Body  
+    console.log(sum);     // Function Body
+})(2,3);
+
+(() => {   // Function Declaration
+    console.log((2+ 3));     // Function Body
+})()
+
+ 
+
