ac53bebd9ec44994beb78dde262cb02bae3f1899
Arsenic <airajena07@gmail.com>
2023-08-02 19:21:58 +0530

    Iterations

---
+++ Javascript/05_Iterations/two.js
@@ -0,0 +1,22 @@
+// while loop 
+let i = 0;
+while (i < 10) {
+    console.log(i);
+    i++;
+}
+
+let myArray = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+let j = 0;
+while (j < myArray.length) {
+    console.log(myArray[j]);
+    j++;
+}
+
+// do while loop
+let k = 0;
+do {
+    console.log(k);
+    k++;
+}
+while (k < 10);
+
