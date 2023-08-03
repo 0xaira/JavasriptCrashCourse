b17c8d3b938d59485ee6fe4782a9f09df5b59e8c
Arsenic <airajena07@gmail.com>
2023-08-03 19:29:20 +0530

    DOM

---
+++ Javascript/06_DOM/script.js
@@ -0,0 +1,40 @@
+//Dom Manipulation
+
+// var head = document.getElementById("heading");
+// console.log(head);
+// head.style.color = "red";
+// head.style.backgroundColor = "yellow";
+// head.style.fontSize = "50px";
+// head.style.border = "2px solid black";
+// head.style.padding = "10px";
+// head.style.margin = "10px";
+// head.style.textAlign = "center";
+// head.style.fontFamily = "sans-serif";
+// head.style.fontWeight = "bold";
+// head.style.borderRadius = "10px";
+// head.style.boxShadow = "0 0 10px black";
+// head.style.textShadow = "0 0 10px black";
+// head.style.width = "50%";
+
+//Differnece between innerText and innerContent
+
+//innerText will not show the hidden text
+//textContent will show the hidden text
+
+// var head = document.getElementById("heading");
+// console.log(head.innerText);
+// console.log(head.textContent);
+
+//innerHTML will show the html tags also and innerText will not show the html tags only text will be shown.
+
+//querySelector
+
+// var head = document.querySelector("#heading");
+// console.log(head);
+
+// var head = document.querySelector(".heading");
+// console.log(head);
+
+// var head = document.querySelector("h1");
+// console.log(head);
+
---
+++ Javascript/06_DOM/script2.js
@@ -0,0 +1,48 @@
+// 4 Pillars of DOM 
+
+// 1. Selection of an element
+// 2. Changing HTML
+// 3. Changing the style of an element
+// 4. Event Listener
+
+let a = document.querySelector("h1");
+a.innerHTML = "<em>Hello World</em>";
+console.log(a.innerHTML);
+a.style.color = "lavender";
+a.style.backgroundColor = "black";
+a.style.fontSize = "50px";
+a.style.border = "2px solid black";
+a.style.padding = "10px";
+a.style.margin = "10px";
+a.style.textAlign = "center";
+a.style.fontFamily = "sans-serif";
+a.style.fontWeight = "bold";
+a.style.borderRadius = "10px";
+a.style.boxShadow = "0 0 10px black";
+a.style.textShadow = "0 0 10px black";
+a.style.width = "50%";
+a.style.display = "inline-block";
+a.style.verticalAlign = "middle";
+a.style.lineHeight = "100px";
+
+
+
+a.addEventListener('click', function (){
+    a.innerHTML = "<em>Hello India</em>";
+    a.style.color = "Green";
+    a.style.backgroundColor = "orange";
+    a.style.fontSize = "50px";
+    a.style.border = "2px solid black";
+    a.style.padding = "10px";
+    a.style.margin = "10px";
+    a.style.textAlign = "center";
+    a.style.fontFamily = "sans-serif";
+    a.style.fontWeight = "bold";
+    a.style.borderRadius = "10px";
+    a.style.boxShadow = "0 0 10px black";
+    a.style.textShadow = "0 0 10px black";
+    a.style.width = "50%";
+    a.style.display = "inline-block";
+    a.style.verticalAlign = "middle";
+    a.style.lineHeight = "100px";
+})
